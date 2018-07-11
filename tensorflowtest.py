93368fde01324178059213f056673ccc26fa8515
Stefan Klas <sk15962@bristol.ac.uk>
2018-07-11 14:59:14 +0100

    A few graphs

--- tensorflowtest.py
+++ tensorflowtest.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pandas as pd
 import tkinter as tk
 import matplotlib.pyplot as plt
+import numpy as np
 
 class DataVisualisation(object):
     fileName    = ""
@@ -57,7 +58,36 @@ class DataVisualisation(object):
         ax = fig.add_subplot(111)
         cax = ax.matshow(correlations, vmin=-1, vmax=1)
         fig.colorbar(cax)
-        plt.savefig("Correlation/correlationMatrix")
+        plt.savefig("Correlation/KPIS")
+
+    def correlation(self):
+        groupedRow = self.dataFrame.groupby(self.dataFrame[self.dataFrame.columns[0]].dt.weekday_name)
+        dayCorrelation = np.zeros((7,7))
+        group1c = 0
+        for name1, group1 in groupedRow:
+            group1 = group1.drop("Period start time", axis=1)
+            group1 = group1.drop("PLMN Name", axis=1)
+            newdataFrame1 = pd.DataFrame()
+            newdataFrame1 = group1.reset_index(drop=True)
+            group2c = 0
+            for name2, group2 in groupedRow:
+                group2 = group2.drop("Period start time", axis=1)
+                group2 = group2.drop("PLMN Name", axis=1)
+                newdataFrame2 = pd.DataFrame()
+                newdataFrame2 = group2.reset_index(drop=True)
+                print (newdataFrame1)
+                print (newdataFrame2)
+                colCorrelation = newdataFrame1.corrwith(newdataFrame2)
+                averageBetweenTwoDays = colCorrelation.mean()
+                dayCorrelation[group1c][group2c] =  averageBetweenTwoDays
+                group2c += 1
+            group1c += 1
+        correlationFrame = pd.DataFrame(dayCorrelation)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.matshow(correlationFrame, vmin=-1, vmax=1)
+        fig.colorbar(cax)
+        plt.savefig("Correlation/Days")
 
 
 
@@ -68,8 +98,8 @@ def main():
     dV1.readDataSheet()
     #dV1.meanAndErrorPlot()
     #dV1.boxPlot()
-    dV1.correlationPlot()
-
+    #dV1.correlationPlot()
+    dV1.correlation()
 
 if __name__== "__main__":
   main()

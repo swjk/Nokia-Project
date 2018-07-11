935d00bab5d3bb590a0439568d12871cdf568273
Stefan Klas <sk15962@bristol.ac.uk>
2018-07-11 14:06:35 +0100

    correlation

--- tensorflowtest.py
+++ tensorflowtest.py
@@ -29,13 +29,46 @@ class DataVisualisation(object):
                     newdataFrame[name] = group[column].reset_index(drop=True)
 
                 newdataFrame.boxplot()
-                plt.savefig("BaselineGraph/" + column)
+                plt.savefig("BoxPlots/" + column)
+
+    def meanAndErrorPlot(self):
+        groupedRow = self.dataFrame.groupby(self.dataFrame[self.dataFrame.columns[0]].dt.weekday_name)
+        print("Plotting")
+        for index,column in enumerate(self.dataFrame):
+            if(index >= 2):
+                plt.figure()
+                plt.title(column)
+                newdataFrame = pd.DataFrame()
+                weekday = 0
+                for name, group in groupedRow:
+                    weekday += 1
+                    newdataFrame[name] = group[column].reset_index(drop=True)
+                    mean     = newdataFrame[name].mean()
+                    std = newdataFrame[name].std()
+                    plt.errorbar(weekday,mean,std,fmt='o')
+                plt.savefig("ErrorBarPlots/" + column)
+
+
+    def correlationPlot(self):
+        self.dataFrame = self.dataFrame.drop("Period start time", axis=1)
+        self.dataFrame = self.dataFrame.drop("PLMN Name", axis=1)
+        correlations = self.dataFrame.corr()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        cax = ax.matshow(correlations, vmin=-1, vmax=1)
+        fig.colorbar(cax)
+        plt.savefig("Correlation/correlationMatrix")
+
+
+
+
 
 def main():
     dV1 = DataVisualisation("ExampleDailycharts.xlsx", "Data")
     dV1.readDataSheet()
-    dV1.boxPlot()
-
+    #dV1.meanAndErrorPlot()
+    #dV1.boxPlot()
+    dV1.correlationPlot()
 
 
 if __name__== "__main__":

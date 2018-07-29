62bc93edd818748ec5b2758a3b8bccb43a2f6e1c
Stefan Klas <sk15962@bristol.ac.uk>
2018-07-29 13:21:01 +0100

    ARIMA done

--- Source/predictor.py
+++ Source/predictor.py
@@ -2,8 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import config as cfg
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.stattools import acf,pacf
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima_model import ARMAResults
 from sklearn import datasets, linear_model
+from scipy.optimize import brute
+
+
 
 class ExportData(object):
     fileName = ""
@@ -41,9 +47,6 @@ class ImportData(object):
             if 'Period start time' in self.dataFrame:
                 self.dataFrame['Period start time'] = pd.to_datetime(self.dataFrame['Period start time'], format="%d/%m/%Y %H:%M:%S", utc=True, errors='coerce')
 
-        def groupDataByDay(self):
-            return self.dataFrame.groupby(self.dataFrame.index.dayofyear, sort=False)
-
         def setDataUp(self):
             #Set datetime to be index
             self.dataFrame.set_index('Period start time', inplace=True)
@@ -58,10 +61,8 @@ class ImportData(object):
             #Make sure columns are numeric
             self.dataFrame = self.dataFrame.apply(pd.to_numeric, errors='ignore')
 
-
-
         def dataSeparation(self, trainingDays):
-            dailyGroupedData = self.groupDataByDay()
+            dailyGroupedData = DataManipulation.groupDataByDay(self.dataFrame)
             firstDay         = next(iter(dailyGroupedData.groups))
             trainingData     = pd.DataFrame()
             forcastData      = pd.DataFrame()
@@ -73,6 +74,14 @@ class ImportData(object):
                     forcastData = forcastData.append(dayReadings)
 
             return (trainingData, forcastData)
+
+class DataManipulation(object):
+    @staticmethod
+    def groupDataByDay(data):
+        return data.groupby(data.index.dayofyear, sort=False)
+    @staticmethod
+    def groupDataByDayName(data):
+            return data.groupby(data.index.weekday, sort=False)
 
 
 class GraphData(object):
@@ -150,16 +159,6 @@ def referenceAlgorithm2(trainingData):
 -------------------------------------------------------------------------------
 First Correlation Algorithm
 """
-def arimaAlgorithm(spreadSheetIn):
-    firstcolumn = pd.Series(spreadSheetIn.dataFrame.iloc[:,0])
-    train = firstcolumn[:20]
-    test = firstcolumn[20:]
-    model = ARIMA(train, order = (2,12,0))
-    model_fit = model.fit(disp= 0)
-    print(model_fit.summary())
-
-
-
 def correlation(spreadSheetIn, spreadSheetOut, upperThreshold):
     #Correlation is always absolute value
     correlations = spreadSheetIn.dataFrame.corr()
@@ -202,6 +201,154 @@ def linearRegressionAlgorithm(trainingData, correlationHourlyData):
     print (subset)
     print (topCorrelation)
 
+"""
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+ARIMA
+"""
+def analyseARIMA(trainingData):
+    targetColumn = trainingData['Avg act UEs DL']
+    lag_acf  = acf(targetColumn,  nlags=170)
+    lag_pacf = pacf(targetColumn, nlags=170, method='ols')
+
+    figure = plt.figure()
+    plt.subplot(121)
+    plt.stem(lag_acf)
+    plt.axhline(y=0, linestyle='-', color='black')
+    plt.axhline(y=-1.96/np.sqrt(len(targetColumn)), linestyle='--', color='gray')
+    plt.axhline(y=1.96/np.sqrt(len(targetColumn)), linestyle='--', color='gray')
+    plt.xlabel('Lag')
+    plt.ylabel('ACF')
+
+
+    plt.subplot(122)
+    plt.stem(lag_pacf)
+    plt.axhline(y=0, linestyle='-', color='black')
+    plt.axhline(y=-1.96/np.sqrt(len(targetColumn)), linestyle='--', color='gray')
+    plt.axhline(y=1.96/np.sqrt(len(targetColumn)), linestyle='--', color='gray')
+    plt.xlabel('Lag')
+    plt.ylabel('PACF')
+
+    plt.tight_layout()
+    plt.show()
+
+def arimaModel(order,day1KPIData):
+    print(order)
+    try:
+        model       = ARIMA(day1KPIData, order=order)
+        model1Result= model.fit(disp=0)
+        residuals = pd.DataFrame(model1Result.resid)
+        print(residuals)
+        if residuals.isnull().values.any():
+            print(10**10)
+            return 10**10
+        else:
+            print("Std")
+            print(residuals.std())
+            return residuals.std()
+    except KeyboardInterrupt:
+        return
+    except:
+        return 10**10
+
+
+
+def analyseARIMA2(trainingData, futureData, predictionHours):
+    #Assuming prediction hours <= 24
+    groupedByDayName = DataManipulation.groupDataByDayName(trainingData)
+    firstDateTime = pd.to_datetime(trainingData.index.values[-1]) + pd.Timedelta(hours=1)
+    timeRangeDay1 = pd.date_range(start=(firstDateTime),
+                              end=pd.Timestamp(year=firstDateTime.year, month=firstDateTime.month, day=firstDateTime.day, hour=23),
+                              freq='H')
+    timeRangeDay2 = pd.date_range(start=pd.Timestamp(year=firstDateTime.year, month=firstDateTime.month, day=firstDateTime.day) + pd.DateOffset(1), periods=predictionHours-len(timeRangeDay1), freq='H')
+
+    forcastLengthDay1 = len(timeRangeDay1)
+    forcastLengthDay2 = len(timeRangeDay2)
+
+    day1Name = timeRangeDay1.weekday[0]
+    day2Name = (day1Name + 1) % 7
+
+    day1Data = groupedByDayName.get_group(day1Name)
+    day2Data = groupedByDayName.get_group(day2Name)
+
+    for kpi in day1Data:
+        day1KPIData = day1Data[kpi]
+        day1KPIDataReset = day1KPIData.reset_index(drop=True)
+        day2KPIData = day2Data[kpi]
+        day2KPIDataReset = day1KPIData.reset_index(drop=True)
+
+        try:
+            grid = (slice(1,8,1), slice(0,3,1), slice(0,3,1))
+            optimizeOrderPara = brute(arimaModel, grid, args=[(day1KPIDataReset)], finish=None)
+            print("OPTIMISED")
+            print(optimizeOrderPara)
+            model1 = ARIMA(day1KPIDataReset, order=(int(optimizeOrderPara[0]),int(optimizeOrderPara[1]),int(optimizeOrderPara[2]))).fit(disp=0)
+
+        except:
+            continue
+
+        predictionDataDay1 = model1.forecast(forcastLengthDay1)
+        predictionDataDay2 = None
+        if forcastLengthDay2 > 0:
+            try:
+                grid = (slice(1,8,1), slice(0,3,1), slice(0,3,1))
+                optimizeOrderPara = brute(arimaModel, grid, args=[(day1KPIDataReset)], finish=None)
+                print("OPTIMISED")
+                print(optimizeOrderPara)
+                model2 = ARIMA(day1KPIDataReset, order=(int(optimizeOrderPara[0]),int(optimizeOrderPara[1]),int(optimizeOrderPara[2]))).fit(disp=0)
+
+            except:
+                continue
+
+            predictionDataDay2 = model2.forecast(forcastLengthDay2)
+
+        predictionDataFrame = pd.DataFrame()
+        if predictionDataDay2 == None:
+            forcastData = predictionDataDay1[0]
+            predictionDataFrame[kpi] = forcastData
+            predictionDataFrame.set_index(timeRangeDay1, inplace=True)
+        else:
+            forcastData = predictionDataDay1[0] + predictionDataDay2[0]
+            predictionDataFrame[kpi] = forcastData
+            predictionDataFrame.set_index(timeRangeDay1.append(timeRangeDay2), inplace =True)
+        GraphData.comparisonPlot([{'data': predictionDataFrame, 'name': 'prediction'},
+                                  {'data':futureData, 'name': 'actual'}], '../New/')
+
+
+
+def test(trainingData):
+    targetColumn = trainingData['Avg act UEs DL']
+    targetData = pd.concat([targetColumn, targetColumn],ignore_index=True)
+    #model = ARIMA(targetColumn, order=(1,0,1))
+    #results_ARIMA = model.fit()
+    #print(results_ARIMA.aic)
+
+
+
+"""
+def seasonalARIMA(trainingData, futureData):
+    targetColumn = trainingData['Avg act UEs DL']
+    targetData = pd.concat([targetColumn, targetColumn],ignore_index=True)
+
+
+
+    #model = SARIMAX(targetData, order= (2,1,1), seasonal_order=(1,0,1,168), enforce_stationarity=False, enforce_invertibility=False)
+    #model_fit = model.fit(disp=False)
+
+    futureForcast = 24
+    predictionData = model_fit.forecast(futureForcast)
+
+    fig = plt.figure()
+    plt.plot(predictionData)
+    plt.plot(futureData['Avg act UEs DL'])
+    plt.show()
+    print (predictionData)
+
+"""
+
+"""
+-------------------------------------------------------------------------------
+"""
 
 def calculateMAE(originalData, predictionData):
     maeFrame = pd.DataFrame()
@@ -244,13 +391,14 @@ def main():
 
     #forcastReference(trainingData, futureData)
 
-    correlationDailyData  = correlation(spreadSheet2In, spreadSheet2Out, cfg.dailyThreshold)
-    correlationHourlyData = correlation(spreadSheet1In, spreadSheet3Out, cfg.hourlyThreshold)
+    #correlationDailyData  = correlation(spreadSheet2In, spreadSheet2Out, cfg.dailyThreshold)
+    #correlationHourlyData = correlation(spreadSheet1In, spreadSheet3Out, cfg.hourlyThreshold)
 
-    linearRegressionAlgorithm(trainingData, correlationHourlyData)
-    #arimaAlgorithm(spreadSheet1In)
-
-
+    #linearRegressionAlgorithm(trainingData, correlationHourlyData)
+    #analyseARIMA(trainingData)
+    #test(trainingData)
+    #seasonalARIMA(trainingData,futureData)
+    analyseARIMA2(trainingData,futureData, 24)
 
 
     #predictionData = referenceAlgorithm(spreadSheet1In, cfg.referenceWindowSize)
